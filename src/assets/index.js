310d2cd2257dcd17f5a9d3c181dbb669cbb1bda6
Armstrong Omoregie <ammycan6@gmail.com>
2025-03-04 13:34:02 +0100

    Navbar 50% & Dynamic Logo

---
+++ src/assets/index.js
@@ -0,0 +1,5 @@
+import mobile_logo from './mobile-logo.png';
+import tablet_logo from './tablet-logo.png';
+import desktop_logo from './desktop-logo.png';
+
+export { mobile_logo, tablet_logo, desktop_logo };
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,14 +1,52 @@
 import { Link } from 'react-router-dom';
 import './navbar.css';
+import { mobile_logo, tablet_logo, desktop_logo } from '../../assets/index';
+import { useEffect, useState } from 'react';
 
 const Navbar = () => {
-  return (
-    <nav className='navbar'>
-      <div className='navbar-logo'>
-        <Link to='/the-movie-flix'>Logo</Link>
-      </div>
-    </nav>
-  )
+
+    // State Management
+    const [logo, setLogo] = useState(mobile_logo);
+
+    // useEffect
+    useEffect(() => {
+        const updateLogo = () => {
+            if (window.innerWidth >= 1024) {
+                setLogo(desktop_logo);
+            } else if (window.innerWidth >= 768) {
+                setLogo(tablet_logo);
+            } else {
+                setLogo(mobile_logo);
+            }
+        };
+
+        updateLogo(); // set initial logo
+        // ✅ Add event listener
+        window.addEventListener('resize', updateLogo);
+
+        // ✅ Cleanup function to remove event listener when component unmounts
+        return () => {
+            window.removeEventListener('resize', updateLogo);
+        };
+    }, [])
+
+
+    return (
+        <nav className='navbar'>
+            <div className='navbar-logo'>
+                <Link to='/the-movie-flix'>
+                    <img
+                        src={logo}
+                        alt="logo"
+                    />
+                </Link>
+            </div>
+            <div className='navbar-links'>
+                <Link to='/the-movie-flix' className='nav-link'>Home</Link>
+                <Link to='/the-movie-flix/favorites' className='nav-link'>Favorites</Link>
+            </div>
+        </nav>
+    )
 }
 
 export default Navbar;
