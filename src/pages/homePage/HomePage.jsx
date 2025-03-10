7b5eccf38cc3e894dd8086b0637f095fc4a301fd
Armstrong Omoregie <ammycan6@gmail.com>
2025-03-10 09:28:03 +0100

    Search History Function, absent sytle and ability to delete history

--- src/pages/homePage/HomePage.jsx
+++ src/pages/homePage/HomePage.jsx
@@ -15,6 +15,9 @@ const HomePage = () => {
     const [page, setPage] = useState(1); // Current page
     const [totalPages, setTotalPages] = useState(1); // Track total pages
     const [selectedMovie, setSelectedMovie] = useState(null);
+    const [searchHistory, setSearchHistory] = useState(
+        JSON.parse(localStorage.getItem("searchHistory")) || []
+    ); //JSON.parse convert retrived JSON string('[]') into an Array([]) object
 
     // UseEffect
     // Movie data object
@@ -80,11 +83,18 @@ const HomePage = () => {
         // keep the form submit from refreshing the page 
         e.preventDefault();
 
+        // searchQuery is the new search term entered by the user
         if (!searchQuery.trim()) {
             return
         };
 
         setPage(1); //Reset to first page on new search
+
+        //Save new search history
+        // [searchQuery, ...searchHistory] creates a new array and the searchQuery is added to the front of the array.
+        const updatedHistory = [searchQuery, ...searchHistory].slice(0, 5); //Keep last 5 searches
+        setSearchHistory(updatedHistory); //Updates the Ui
+        localStorage.setItem("searchHistory", JSON.stringify(updatedHistory)); //Converts a JavaScript value(Array/Object) to a JavaScript Object Notation (JSON) string.
     };
 
     // Function to open modal with details of clicked movie
@@ -110,11 +120,26 @@ const HomePage = () => {
                     <input
                         type="text"
                         placeholder='Search for movies...' className='search-input'
-                        value={searchQuery}
                         onChange={(e) => (setSearchQuery(e.target.value))}
+                        value={searchQuery}
                     />
 
-                    <button type='submit' className='search-button'>Search</button>
+                    <button
+                        type='submit' className='search-button'
+                    >Search</button>
+
+                    {/* Search History Dropdown */}
+                    {/* True if search history (from localStorage) is not empty*/}
+                    {searchHistory.length > 0 && (
+                        <div className='search-history'>
+                            {searchHistory.map((pastQuery, index) => (
+                                <div
+                                    key={index}
+                                    onClick={() => setSearchQuery(pastQuery)}
+                                >{pastQuery}</div>
+                            ))}
+                        </div>
+                    )}
                 </form>
 
                 {/* Error message */}
